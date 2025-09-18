4cf32a581c3720b8d46b28e6a9696ff1024e911c
Joshua <noll.joshua@outlook.com>
2025-09-18 16:32:08 -0400

    chore: change auth controller import names

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const authCtl = require('../controllers/auth.js')
 
-router.get('/login', subscribeCtl.userLogin)
-router.get('/login', subscribeCtl.userLogout)
+router.get('/login', authCtl.userLogin)
+router.get('/logout', authCtl.userLogout)
 
 module.exports = router
