a2eaea9cc0fc8d3decf1fb665fc7cf45753842f9
Joshua <noll.joshua@outlook.com>
2025-09-20 20:23:49 -0400

    fix: successfully checking whether topic is already subscribed to

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const port = process.env.API_PORT || 3001;
 const subscribeRoutes = require('./routes/subscribe.js')
 const authRoutes = require('./routes/auth.js')
 
+const mqtt_url = process.env.MQTT_URL || 'mqtt://localhost:1883'
+const mqtt = require("mqtt")
+const mqttClient = mqtt.connect(mqtt_url)
+
+const mongoose = require('mongoose');
+const mongo_host = process.env.MONGO_HOST || 'localhost'
+const db = process.env.DB_NAME || 'black-relay'
+const mongo_user = process.env.MONGO_USER || 'admin'
+const mongo_pass = process.env.MONGO_PASS || 'password'
+const db_url = `mongodb://${mongo_user}:${mongo_pass}@${mongo_host}:27017/${db}?authSource=admin`
+const mongooseConn = mongoose.connect(db_url)
+.then(() => console.log(`Connected to MongoDB at URL ${db_url}`))
+.catch((err) => console.error(`Unable to connect to MongoDB at URL ${db_url}. Message:\n${err}`))
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
@@ -41,3 +55,8 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   console.log(`API URL: http://localhost:${port}`);
 });
+
+module.exports = [
+  mongooseConn,
+  mqttClient
+]
--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -16,36 +16,58 @@ mqtt_client.on("connect", () => {
   console.log(`Server connected to MQTT server at URL ${mqtt_url}`)
 })
 
-exports.subscribeToTopic = (req, res) => {
-  mqtt_client.subscribe(req.params.topic, (err) => {
-    if (err){
-      console.error(err)
-      res.status(404).json({
-        status: "failed",
-        message: `Could not subscribe to topic ${req.params.topic}`
-      })
-    }
-    else{
-      const Col = mongoose.model(req.params.topic, new mongoose.Schema({}, { strict: false }))
-      Col.createCollection()
-      .then( (collection) => {
-        console.log(`Mongo collection ${collection} created`)
-        console.log(`Subscribed to topic ${req.params.topic}`)
-        res.status(200).json({
-          status: "success",
-          message: `Successfully subscribed to topic ${req.params.topic}`
+// TODO - add error handling for if topic is already subscribed to
+exports.subscribeToTopic = async (req, res) => {
+
+  const requestedTopic = req.params.topic
+
+  const subscribed = await mongoose.connection.listCollections()
+  .then(collections => {
+    for (let collection of collections){
+      // Add 's' for comparison since MongoDB adds an 's' to all collection names
+      if (collection.name ===  (requestedTopic + 's')){
+        res.status(409).json({
+          "status": "failed",
+          "message": `Already subscribed to topic ${requestedTopic}`
         })
-      })
-      .catch(err => console.error(`Unable to create collection for ${req.params.topic}. Error:\n${err}`))
-      mqtt_client.on("message", (topic, message) => {
-        if(topic === req.params.topic){
-          message = JSON.parse(message.toString())
-          console.log(message)
-          Col.create(message)
-          .then(doc => console.log('Document saved: ', doc))
-          .catch(err => console.error('Error while saving document: ', err))
-        }
-      })
+        return true
+      }
     }
+    return false
   })
+
+  if (!subscribed){
+    mqtt_client.subscribe(requestedTopic, (err) => {
+      if (err){
+        console.error(err)
+        res.status(400).json({
+          status: "failed",
+          message: `Could not subscribe to topic ${requestedTopic}`
+        })
+      }
+      else{
+        const Model = mongoose.model(requestedTopic, new mongoose.Schema({}, { strict: false }))
+        Model.createCollection()
+        .then( (collection) => {
+          console.log(`Mongo collection ${collection} created`)
+          console.log(`Subscribed to topic ${requestedTopic}`)
+          res.status(200).json({
+            status: "success",
+            message: `Successfully subscribed to topic ${requestedTopic}`
+          })
+        })
+        .catch(err => console.error(`Unable to create collection for ${requestedTopic}. Error:\n${err}`))
+        mqtt_client.on("message", (topic, message) => {
+          if(topic === requestedTopic){
+            message = JSON.parse(message.toString())
+            console.log(message)
+            Col.create(message)
+            .then(doc => console.log('Document saved: ', doc))
+            .catch(err => console.error('Error while saving document: ', err))
+          }
+        })
+      }
+    })
+  }
+
 }
