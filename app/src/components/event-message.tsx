9c2d0f0a6946485566cc71615eca7549f61cf15f
yepitsaname <fernando.g.berry@gmail.com>
2025-10-02 14:37:32 -0400

    adds new message types/components to event-message

--- app/src/components/event-message.tsx
+++ app/src/components/event-message.tsx
@@ -4,6 +4,23 @@ export interface ShortEvent {
   message: string;
   time: string;
 }
+
+export interface LongEvent{
+  active: boolean;
+  acknowledged: boolean;
+  message: string;
+  date: string;
+  time: string;
+  category: string;
+  sensor: string;
+  latitude: string;
+  longitude: string;
+}
+
+export interface DetailedEvent{
+
+}
+
 export function EventMessage({event}:{event: ShortEvent}){
   const { active, acknowledged, message, time } = event;
 
@@ -13,4 +30,27 @@ export function EventMessage({event}:{event: ShortEvent}){
     <td>{message}</td>
     <td>{time}</td>
   </tr>)
+}
+
+export function LongEventMessage({event}:{event: LongEvent}){
+  const { active, acknowledged, message, date, time, category, sensor, latitude, longitude } = event;
+
+  return (<tr>
+    <td>{active ? "Yes" : "No"}</td>
+    <td>{acknowledged ? "Yes" : "No"}</td>
+    <td>{date}</td>
+    <td>{time}</td>
+    <td>{category}</td>
+    <td>{sensor}</td>
+    <td>{latitude} x {longitude}</td>
+    <td>{message}</td>
+  </tr>)
+}
+
+export function EventDetail({event}:{event: DetailedEvent}){
+  const {} = event;
+
+  return (<>
+
+  </>)
 }
