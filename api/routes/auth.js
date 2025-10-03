4b68b550378dacfe86ea359a7223b7de34708ff6
Joshua <noll.joshua@outlook.com>
2025-10-03 17:02:17 -0400

    fix: convert /auth/login endpoint from GET to POST request

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const authCtl = require('../controllers/auth.js')
 
-router.get('/login', authCtl.userLogin)
+router.post('/login', authCtl.userLogin)
 router.get('/logout', authCtl.userLogout)
 
 module.exports = router
