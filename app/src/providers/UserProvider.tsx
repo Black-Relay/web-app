e04a5c4366a18fc6ba7d89cdddc33a59fb1e41c3
yepitsaname <fernando.g.berry@gmail.com>
2025-10-07 11:05:56 -0400

    enabled user check

--- app/src/providers/UserProvider.tsx
+++ app/src/providers/UserProvider.tsx
@@ -2,6 +2,7 @@ import React, { useContext, createContext, useState } from "react";
 
 type User = {
   username: string;
+  role: string;
 };
 
 interface UserContextType {
@@ -18,7 +19,7 @@ function useUserContext():UserContextType{
 };
 
 export default function UserProvider({children}:{children: React.ReactNode}){
-  const [user, setUser] = useState({username: ""});
+  const [user, setUser] = useState({username: "", role: ""});
   const value = {
     user: user,
     setUser: setUser
