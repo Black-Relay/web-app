ced00db78356e4f16a757208690995eab565b3c8
yepitsaname <fernando.g.berry@gmail.com>
2025-10-10 16:12:57 -0400

    feat: removed table for events and converted to tiles

--- app/src/components/event-aside.tsx
+++ app/src/components/event-aside.tsx
@@ -6,18 +6,6 @@ import { mockEvents } from "@/mockdata/mock-events";
 export function EventsAside(){
 
   return (<div className="table-wrapper">
-    <table className="dashboard-event-table">
-      <thead>
-        <tr>
-          <th>Active</th>
-          <th>Ack</th>
-          <th>Message</th>
-          <th>Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        {mockEvents.map((message, index)=><EventMessage key={index} event={message}/>)}
-      </tbody>
-    </table>
+    {mockEvents.map((message, index)=><EventMessage key={index} event={message}/>)}
   </div>)
 }
