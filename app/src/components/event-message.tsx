b19f7c39516570cb41002c914e528968e5bfa1eb
yepitsaname <fernando.g.berry@gmail.com>
2025-10-12 13:20:35 -0400

    fix: corrected time on display to show hh:mm:ss instead of a full ISO date string

--- app/src/components/event-message.tsx
+++ app/src/components/event-message.tsx
@@ -1,6 +1,11 @@
 import { type Event } from "@/providers/EventProvider";
 import "../css/event-message.css";
 
+function convertISODateString(isoDate:string){
+  const date = new Date(isoDate);
+  return date.toLocaleTimeString('en-gb');
+}
+
 export function EventMessage({event}:{event: Event}){
   const { active, acknowledged, category, data, createdAt } = event;
 
@@ -9,7 +14,7 @@ export function EventMessage({event}:{event: Event}){
       <div className={"lamp" + (active ? " active" : "")}></div>
       <div className={"lamp" + (acknowledged ? " ack" : " unack")}></div>
     </div>
-    <div className="timestamp">{createdAt}</div>
+    <div className="timestamp">{convertISODateString(createdAt)}</div>
     <div className="event-message">
       {`[Sensor Name] - `}
       <span className={category.toLowerCase()}>{category}</span>
