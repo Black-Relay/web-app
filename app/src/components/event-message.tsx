b981677368934f5d047b5f673b060f9d2798c139
yepitsaname <fernando.g.berry@gmail.com>
2025-10-12 13:02:45 -0400

    chore: removed extra comments

--- app/src/components/event-message.tsx
+++ app/src/components/event-message.tsx
@@ -16,36 +16,4 @@ export function EventMessage({event}:{event: Event}){
       {`\n[Some message]`}
     </div>
   </div>)
-
-
-  // <tr>
-  //   <td>{active ? "Yes" : "No"}</td>
-  //   <td>{acknowledged ? "Yes" : "No"}</td>
-  //   <td>{`[Sensor Name] - ${category}\n[Some message]`}</td>
-  //   <td>{createdAt}</td>
-  // </tr>
-
 }
-
-// export function LongEventMessage({event}:{event: Event}){
-//   const { active, acknowledged, data, category } = event;
-
-//   return (<tr>
-//     <td>{active ? "Yes" : "No"}</td>
-//     <td>{acknowledged ? "Yes" : "No"}</td>
-//     <td>{}</td>
-//     <td>{}</td>
-//     <td>{category}</td>
-//     <td>{""}</td>
-//     <td>{"Lat"} x {"Long"}</td>
-//     <td>{"Some message"}</td>
-//   </tr>)
-// }
-
-// export function EventDetails({event}:{event: Event}){
-//   const {} = event;
-
-//   return (<>
-
-//   </>)
-// }
