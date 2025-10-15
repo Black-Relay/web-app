6697754cbfc42117dfab673311a9114249f07ac4
yepitsaname <fernando.g.berry@gmail.com>
2025-10-15 15:32:03 -0400

    chore: adding lamps to commit

--- app/src/components/ui/lamp.tsx
+++ app/src/components/ui/lamp.tsx
@@ -1,4 +1,25 @@
+import type React from "react";
 import "../../css/lamp.css";
-export function Lamp({state = ""}:{state?: ""|"active"|"unack"|"ack"}){
+
+type LampState = ""|"active"|"unack"|"ack";
+
+export function Lamp({state = ""}:{state?: LampState}){
   return <div className={`lamp ${state}`}></div>
+}
+
+export function LampLabel({label, children}:{label:string, children:React.ReactNode}){
+  return (
+    <div className="lamp-wrapper">
+      <p>{label}</p>
+      {children}
+    </div>
+  )
+}
+
+export function VerticalLamps({children}:{children:React.ReactNode}){
+  return <div className="vertical-lamps">{children}</div>
+}
+
+export function HorizontalLamps({children}:{children:React.ReactNode}){
+  return <div className="horizontal-lamps">{children}</div>
 }
