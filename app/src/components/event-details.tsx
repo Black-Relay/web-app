122e3b9128938011edaa547da9f19f7a8f86fe79
yepitsaname <fernando.g.berry@gmail.com>
2025-10-16 14:35:37 -0400

    feat: added UI section buttons - no functionality as of yet

--- app/src/components/event-details.tsx
+++ app/src/components/event-details.tsx
@@ -2,9 +2,11 @@ import type { Event } from "@/providers/EventProvider";
 import { HorizontalLamps, LampLabel, Lamp } from "./ui/lamp";
 import "../css/event-message.css";
 import { useState } from "react";
-import { Note, NoteList, type NoteData } from "./ui/note";
+import { NoteList, type NoteData } from "./ui/note";
+import { Notebook, Pin, Search } from "lucide-react"
 
 import { mockNotes } from "@/mockdata/mock-notes";
+import { IconButton } from "./ui/icon-button";
 
 interface EventNotes {
   eventID: string;
@@ -16,11 +18,11 @@ function EventMetaSection({event}:{event:Event}) {
 
   return (
     <div className="section">
-      <p><span className="bold">ID:</span> &nbsp;&nbsp;{_id}</p>
-      <p><span className="bold">Time:</span> &nbsp;&nbsp;{createdAt}</p>
-      <p><span className="bold">Type:</span> &nbsp;&nbsp;<span className={category.toLowerCase()}>{category}</span></p>
-      <p><span className="bold">Topic:</span> &nbsp;&nbsp;{topic}</p>
-      <p><span className="bold">Status:</span> &nbsp;&nbsp;
+      <p><span className="bold">ID:</span>&nbsp;&nbsp;{_id}</p>
+      <p><span className="bold">Time:</span>&nbsp;&nbsp;{createdAt}</p>
+      <p><span className="bold">Type:</span>&nbsp;&nbsp;<span className={category.toLowerCase()}>{category}</span></p>
+      <p><span className="bold">Topic:</span>&nbsp;&nbsp;{topic}</p>
+      <p><span className="bold">Status:</span>&nbsp;&nbsp;
         <HorizontalLamps>
           <LampLabel label="Ack'd">
             <Lamp state={acknowledged ? "ack" : "unack"} />
@@ -46,7 +48,9 @@ function EventNoteSection({eventNotes}:{eventNotes:EventNotes}){
 function EventUISection(){
   return (
     <div className="section">
-      UI Section
+      <IconButton Icon={Search} label="View Event Data" method={()=>{}} />
+      <IconButton Icon={Notebook} label="Add Note" method={()=>{}} />
+      <IconButton Icon={Pin} label="Pin Event" method={()=>{}} />
     </div>
   )
 }
