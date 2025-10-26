3b3673f82200b6eaba34a71e495c50bbf838915f
Joshua <noll.joshua@outlook.com>
2025-10-25 21:39:11 -0400

    fix: broken mongo environment variable name

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongo_host = process.env.MONGO_HOST || 'localhost'
 const db_name = process.env.DB_NAME || 'black-relay'
 const mongo_user = process.env.MONGO_USER || 'admin'
-const mongo_pass = process.env.MONGO_PASS || 'password'
+const mongo_pass = process.env.MONGO_PASSWORD || 'password'
 const db_url = `mongodb://${mongo_user}:${mongo_pass}@${mongo_host}:27017/${db_name}?authSource=admin`
 const mongooseConn = mongoose.connect(db_url)
 .then(() => console.log(`Connected to MongoDB at ${db_url}`))
