5e3b9202413740d7f209680fde2fc2c0c4cfb7af
yepitsaname <fernando.g.berry@gmail.com>
2025-11-12 17:51:44 -0500

    feat: updated category switch to control table

--- app/src/pages/Events.tsx
+++ app/src/pages/Events.tsx
@@ -6,7 +6,7 @@ import { EventTable } from "@/components/event-table";
 import { useEventContext } from "@/providers/EventProvider";
 
 export function Events(){
-  const [table, setTable] = useState("All");
+  const [table, setTable] = useState<string>("");
   const { events } = useEventContext();
   const [ selectedEvent, setSelectedEvent ] = useState<any>(events[0]);
   const columnNames = [
@@ -18,6 +18,19 @@ export function Events(){
     { key: "topic", header: "GROUP", sortable: true }
   ]
 
+  function getCategoryFromSwitch(switchLabel: string): string | undefined {
+    switch (switchLabel.toUpperCase()) {
+      case "DETECTS": return "DETECT";
+      case "ALERTS": return "ALERT";
+      case "THREATS": return "THREAT";
+      case "ALARMS": return "ALARM";
+      default: return "";
+    }
+  }
+
+  const filteredEvents = getCategoryFromSwitch(table) === "" ? events
+      : events.filter(ev => (ev.category || "") === getCategoryFromSwitch(table));
+
   return <div className="layout-main-content no-footer">
     <header>
       <h1>Events</h1>
@@ -25,7 +38,7 @@ export function Events(){
     <MissionClock />
     <main>
       <Switch labels={["All","Detects","Alerts","Threats","Alarms"]} setSwitch={setTable}/>
-      <EventTable columns={columnNames} data={events} setEvent={setSelectedEvent} />
+      <EventTable columns={columnNames} data={filteredEvents} setEvent={setSelectedEvent} />
     </main>
     <EventDetailsPane event={selectedEvent}/>
   </div>
