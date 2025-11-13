a37be65a35f0330c486fef6a7574ff01bbd39d28
yepitsaname <fernando.g.berry@gmail.com>
2025-11-13 02:38:22 -0500

    feat: adds the ability for the user to acknowledge alarms from the front panel

--- app/src/components/event-message.tsx
+++ app/src/components/event-message.tsx
@@ -1,6 +1,9 @@
 import { type Event } from "@/providers/EventProvider";
 import "../css/event-message.css";
 import { Lamp, VerticalLamps } from "./ui/lamp";
+import { useState } from "react";
+import config from "../configs/config.json";
+const {apiUrl} = {apiUrl: import.meta.env.VITE_API_URL || config.apiUrl}
 
 function convertISODateString(isoDate:string){
   const date = new Date(isoDate);
@@ -8,12 +11,39 @@ function convertISODateString(isoDate:string){
 }
 
 export function EventMessage({event}:{event: Event}){
-  const { active, acknowledged, topic, category, data, createdAt } = event;
+  const {_id, active, acknowledged, topic, category, data, createdAt } = event;
+  const [isAck, setIsAck] = useState(acknowledged);
+
+    const handleAcknowledge = async () => {
+    if (!isAck) {
+      try {
+        const res = await fetch(`${apiUrl}/event/id/${_id}`, {
+          method: "PATCH",
+          credentials: "include",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ acknowledged: true })
+        });
+        if (res.ok) setIsAck(true);
+      } catch (err) {
+        alert(`Unable to acknowledge ${_id}`)
+      }
+    }
+  };
 
   return (<div className="event-tile">
     <VerticalLamps>
       {/* <Lamp state={active ? "active" : ""} /> */}
-      <Lamp state={acknowledged ? "ack" : "unack"} />
+      <button
+        className="lamp-btn"
+        aria-label={isAck ? "Acknowledged" : "Acknowledge event"}
+        onClick={handleAcknowledge}
+        disabled={isAck}
+        style={{ background: "none", border: "none", padding: 0, cursor: isAck ? "default" : "pointer" }}
+      >
+        <Lamp state={isAck ? "ack" : "unack"} />
+      </button>
     </VerticalLamps>
     <div className="timestamp">{convertISODateString(createdAt)}</div>
     <div className="event-message">
