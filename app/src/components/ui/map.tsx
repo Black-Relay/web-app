7f2841c7bbbdb30f9a2154494a20775f77eca1b8
Joshua <noll.joshua@outlook.com>
2025-11-14 12:48:35 -0500

    update osm URL

--- app/src/components/ui/map.tsx
+++ app/src/components/ui/map.tsx
@@ -1,34 +1,40 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../../css/map.css";
-import { useEventContext, type Event } from '@/providers/EventProvider';
+import { useEventContext, type Event } from "@/providers/EventProvider";
+import config from "../../configs/config.json";
+const { osmUrl } = { osmUrl: import.meta.env.VITE_OSM_URL || config.osmUrl };
 
-function Pin({event}:{event:Event}){
-  const {category, topic, createdAt, data} = event;
-  let latitude = data?.LAT as number || 35.7796;
-  let longitude = data?.LON as number || -78.6382;
+function Pin({ event }: { event: Event }) {
+  const { category, topic, createdAt, data } = event;
+  let latitude = (data?.LAT as number) || 35.7796;
+  let longitude = (data?.LON as number) || -78.6382;
 
   return (
     <Marker position={[latitude, longitude]}>
       <Popup>
-        <span className={category.toLowerCase()}>{category}</span> - <span className='sentence'>{topic}</span> <br />
+        <span className={category.toLowerCase()}>{category}</span> -{" "}
+        <span className="sentence">{topic}</span> <br />
         {createdAt} <br />
-        Coords: {latitude},  {longitude} <br />
+        Coords: {latitude}, {longitude} <br />
         [data]
       </Popup>
     </Marker>
-  )
+  );
 }
 
-export function Map(){
+export function Map() {
   const { events } = useEventContext();
-  return(
+  return (
     <MapContainer center={[35.28, -79.64]} zoom={8} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="http://localhost:8080/tile/{z}/{x}/{y}.png"
+        url={`${osmUrl}/tile/{z}/{x}/{y}.png`}
       />
-      {events.map((e, i) => <Pin key={`${e.topic}-${e.createdAt}-${i}`} event={e} />)}
+      {events.map((e, i) => (
+        <Pin key={`${e.topic}-${e.createdAt}-${i}`} event={e} />
+      ))}
     </MapContainer>
-  )
+  );
 }
+
