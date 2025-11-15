3e1aba7ec1e98be9888e8f202f56fde568eb709b
Joshua <noll.joshua@outlook.com>
2025-11-15 08:58:46 -0500

    hide sidebar on landing page

--- app/src/layouts/Layout.tsx
+++ app/src/layouts/Layout.tsx
@@ -1,16 +1,19 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 // import type React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import AppSidebar from "@/components/app-sidebar";
 import AppBreadcrumb from "@/components/app-breadcrumb";
 import "../css/layout.css";
 
 export function Layout() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
   return (
       <SidebarProvider>
-        <AppSidebar />
+        {!isLandingPage && <AppSidebar />}
         <div className="layout">
-          <AppBreadcrumb />
+          {!isLandingPage && <AppBreadcrumb />}
           <Outlet />
         </div>
       </SidebarProvider>
